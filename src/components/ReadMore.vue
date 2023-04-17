8a7e4925b0f493cdf00a4d6e4097864bf237b391
RaulMartinezF5 <raulmartinezalvarez1980@gmail.com>
2023-04-17 09:35:37 +0200

    readmore

--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -1,21 +1,20 @@
-<script setup>
-import { ref } from "vue";
-
-const title = ref("Exercise Read More");
-function showText() {
-  let p = document.querySelector(".cut-text");
-  p.style = `
-  overflow: inherit;
-  white-space: break-spaces;
-  `;
-}
-</script>
-
+    <script setup>
+    import { ref } from "vue";
+    
+    const title = ref("Exercise Read More");
+    const isExpanded = ref(false);
+    const buttonText = ref("Show More");
+    
+    function toggleExpanded() {
+      isExpanded.value = !isExpanded.value;
+      buttonText.value = isExpanded.value ? "Show Less" : "Show More";
+    }
+    </script>
 <template>
   <div class="container text-center">
     <h1 class="my-3">{{ title }}</h1>
     <div>
-      <p class="mb-3 d-block cut-text">
+      <p class="mb-3 d-block" :class="{ 'cut-text': !isExpanded, 'full-text': isExpanded }">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
         ipsa neque odio consectetur impedit tempore modi accusantium! Voluptatem
         molestiae nemo inventore soluta corrupti. Culpa perferendis sapiente
@@ -23,10 +22,20 @@ function showText() {
         temporibus pariatur ab repellendus dignissimos natus aliquam odit ullam,
         quaerat autem non hic, quidem dolore laboriosam.
       </p>
-      <button class="btn btn-primary" @click="showText()">Show More</button>
+      <button class="btn btn-primary" @click="toggleExpanded">{{ buttonText }}</button>
     </div>
     <hr class="mb-4" />
   </div>
 </template>
 
-<style lang="css"></style>
+
+<style lang="css">
+.full-text {
+  overflow: inherit;
+  white-space: break-spaces;
+}
+
+.full-text + button {
+  display: none;
+}
+</style>
