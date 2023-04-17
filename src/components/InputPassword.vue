8f8287756d03db736214a92bbfe0d3c495419bff
RaulMartinezF5 <raulmartinezalvarez1980@gmail.com>
2023-04-17 09:27:05 +0200

    first commit

---
+++ src/components/InputPassword.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+function input() {
+  let input = document.getElementById("password");
+  if (input.type === "password") {
+    input.type = "text";
+  } else {
+    input.type = "password";
+  }
+}
+
+</script>
+
+<template>
+    <div class="container text-center input-password">
+        <h1 class="my-3">Exercise Input Password</h1>
+        <input type="password" name="" id="password" class="form-control">
+        <div class="d-block">
+            <span id="show-password" class="mt-2 btn btn-primary" @click="input()">Show Password</span>
+        </div>
+    </div>
+</template>
+
+<style lang="css" scoped>
+    #show-password:hover {
+        cursor: pointer;
+        user-select: none;
+    }
+</style>
---
+++ src/components/ReadMore.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from "vue";
+
+const title = ref("Exercise Read More");
+function showText() {
+  let p = document.querySelector(".cut-text");
+  p.style = `
+  overflow: inherit;
+  white-space: break-spaces;
+  `;
+}
+</script>
+
+<template>
+  <div class="container text-center">
+    <h1 class="my-3">{{ title }}</h1>
+    <div>
+      <p class="mb-3 d-block cut-text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        ipsa neque odio consectetur impedit tempore modi accusantium! Voluptatem
+        molestiae nemo inventore soluta corrupti. Culpa perferendis sapiente
+        deserunt aperiam maxime consectetur recusandae quasi commodi dicta
+        temporibus pariatur ab repellendus dignissimos natus aliquam odit ullam,
+        quaerat autem non hic, quidem dolore laboriosam.
+      </p>
+      <button class="btn btn-primary" @click="showText()">Show More</button>
+    </div>
+    <hr class="mb-4" />
+  </div>
+</template>
+
+<style lang="css"></style>
